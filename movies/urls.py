1f039a6776017bd77b63f872103a4ae0a44aaded
Che.El <che.eldar@gmail.com>
2020-02-24 19:35:50 +0300

    movie detail view and template page

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MovieView.as_view(), name='movie_list_url'),
+    path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail_url'),
 ]
