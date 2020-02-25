895c75c348a136f42d0c1686622c2fc000746075
Che.El <che.eldar@gmail.com>
2020-02-25 07:30:21 +0300

    movie shots and reviews for movie

--- movies/urls.py
+++ movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.MovieView.as_view(), name='movie_list_url'),
     path('<slug:slug>/', views.MovieDetailView.as_view(), name='movie_detail_url'),
+    path('review/<int:pk>/', views.AddReview.as_view(), name='add_review_url'),
 ]
